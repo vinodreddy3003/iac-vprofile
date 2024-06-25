18eb497344322df1b43e812e3a0874e035e2c687
vinod <vinod20010330@gmail.com>
2024-06-26 02:06:58 +0530

    commitsss

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.30.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.5"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.3"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.23.1"
     }
   }
 
@@ -32,8 +32,5 @@ terraform {
     region = "us-east-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.6.4"
 }
-##
-##
-##
